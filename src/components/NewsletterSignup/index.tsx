8099d1ce20eba0821173520cca3e11c15f9dc952
Harry Lee <hhylee88@gmail.com>
2024-03-17 21:36:22 -0700

    Embedded Beehiiv iframe for email newsletter subscription

--- src/components/NewsletterSignup/index.tsx
+++ src/components/NewsletterSignup/index.tsx
@@ -1,3 +1,25 @@
+//Old NewsletterSignup
+
+// // src/components/NewsletterSignup/index.tsx
+
+// import React from 'react';
+// import styles from './styles.module.css';
+
+// export default function NewsletterSignup(): JSX.Element {
+//   return (
+//     <section className={styles.newsletterSignup}>
+//       <h2>Stay updated with LeetPython and exlcusive contents</h2>
+//       <form>
+//         <input type="email" placeholder="Your Email Address" className={styles.inputField} />
+//         <button type="submit" className={styles.subscribeButton}><b>Subscribe</b></button>
+//       </form>
+//     </section>
+//   );
+// }
+
+
+//New NewsletterSignup that embeds the Beehiiv iframe for subscription.
+
 // src/components/NewsletterSignup/index.tsx
 
 import React from 'react';
@@ -6,11 +28,20 @@ import styles from './styles.module.css';
 export default function NewsletterSignup(): JSX.Element {
   return (
     <section className={styles.newsletterSignup}>
-      <h2>Stay updated with LeetPython and exlcusive contents</h2>
-      <form>
-        <input type="email" placeholder="Your Email Address" className={styles.inputField} />
-        <button type="submit" className={styles.subscribeButton}><b>Subscribe</b></button>
-      </form>
+      <h2>Stay updated with LeetPython and exclusive contents</h2>
+      {/* Embed the Beehiiv iframe for subscription */}
+      <iframe
+        src="https://embeds.beehiiv.com/a5bd3d3c-5bc4-406b-a7ea-372f92f8ad94?slim=true"
+        data-test-id="beehiiv-embed"
+        height="100"
+        style={{
+          margin: 0,
+          borderRadius: '0px',
+          backgroundColor: 'transparent',
+          overflow: 'hidden',
+          // border: 'none'
+        }}
+      ></iframe>
     </section>
   );
 }
