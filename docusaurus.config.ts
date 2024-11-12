392e0ef6322f3df8bb89d9728167cb169d83cfb8
Wayne Lee <wlee@CZIMACOS4875.local>
2024-11-11 21:46:27 -0800

    updated project links in docs/intro

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   organizationName: 'wayne923', // Usually your GitHub org/user name.
   projectName: 'leetpython', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
